a97419b060d65d045c50a1573f16e4514dd221b4
Gilvin Zalsos <gilvinsz@gmail.com>
2022-06-17 18:53:41 +0800

    First try in rearranging survey form UI

--- resources/views/surveyform.blade.php
+++ resources/views/surveyform.blade.php
@@ -8,51 +8,49 @@
    <!--  @foreach($survey as $survey)
     <a href="/createanswer/{{$survey}}"> <button class="bg-yellow-300"> Die and Dump </button> </a>
     @endforeach -->
-
-   
+  
     @foreach($SurveyQuestions as $SurveyQuestion)
 
     @if($SurveyQuestion->type =='multiplechoice')
+  
     <form method="POST" action="/createanswer/{{$survey}}/{{$SurveyQuestion->id}}/"> 
        @csrf
        @method('POST')
-       <div>
-            
-        </div>                                      
-                    <div class="flex-auto space-y-2"> 
-                                       
-                    {{$SurveyQuestion->question}} (Multiple Choice)
-                    
-                    @foreach($choices as $choice)
-                    <input type="checkbox" name="pets" value="Dog"> {{$choice->question_choice}}<br>
-                  
-                    @endforeach
+                                          
+                <div class="flex-auto space-y-2 bg-gray-300"> 
+
+  
+                        <table class="border border-red text-white">                     
+                                <tr class="border border-black" >  
+                                        <th> {{$SurveyQuestion->question}} (Multiple Choice) </th> </tr>
+                                @foreach($choices as $choice)
+                                <th> <input type="checkbox" name="pets" value="Dog"> {{$choice->question_choice}} </th> </tr>          
+                                @endforeach
+                                  </table>
+                        
                                                                          						
-            <button type="submit" 
-                    
-                    class="bg-red-300 text-white rounded ml-1 py-4 px-2 hover:bg-red-500">
-                     Submit Answer 
-                     </button>  
-                    </div>
+            <button type="submit" class="bg-red-300 text-white rounded ml-1 py-4 px-2 hover:bg-red-500"> Submit Answer </button>  
+                     
+                       
+
+                </div>
+                        <div class="bg-red-300">
                     {{$SurveyQuestions->links()}}
-
+                        </div>
                     </form>	
+       
         @endif
-
 
 
      @if($SurveyQuestion->type =='likertscale')
        <form method="POST" action="/createanswer/{{$survey}}/{{$SurveyQuestion->id}}/"> 
        @csrf
        @method('POST')
-       <div>
-           
-        </div>                                      
+                                        
                     <div class="flex-auto space-y-2"> 
                                        
                     {{$SurveyQuestion->question}} (Likert Scale)
                                                           
-
                     @foreach($choices as $choice)
                     <input type="checkbox" name="pets" value="Dog"> {{$choice->question_choice}}<br>
                   
@@ -73,14 +71,11 @@
         <form method="POST" action="/createanswer/{{$survey}}/{{$SurveyQuestion->id}}/"> 
        @csrf
        @method('POST')
-       <div>
-           
-        </div>                                      
+                                          
                     <div class="flex-auto space-y-2"> 
                                        
                     {{$SurveyQuestion->question}} (Rating Scale)
                                                           
-
                     @foreach($choices as $choice)
                     <input type="checkbox" name="pets" value="Dog"> {{$choice->question_choice}}<br>
                   
@@ -101,14 +96,11 @@
         <form method="POST" action="/createanswer/{{$survey}}/{{$SurveyQuestion->id}}/"> 
        @csrf
        @method('POST')
-       <div>
-           
-        </div>                                      
+                                            
                     <div class="flex-auto space-y-2"> 
                                        
                     {{$SurveyQuestion->question}}  (Matrix Question)
                                                           
-
                     @foreach($choices as $choice)
                     <input type="checkbox" name="pets" value="Dog"> {{$choice->question_choice}}<br>
                   
@@ -130,16 +122,11 @@
 <form method="POST" action="/createanswer/{{$survey}}/{{$SurveyQuestion->id}}/"> 
        @csrf
        @method('POST')
-    
-                                      
+                                       
                     <div class="flex-auto space-y-2"> 
-                    
-                    
+                                    
                     {{$SurveyQuestion->question}} (Open Ended)
-                   
-                   
-                    
-
+                                                   
                     <input class="border border-gray-400 p-0.5 w-full"
 			        type="text"
 			        name="answer"
@@ -147,11 +134,7 @@
 		        	required
                     value="{{old('answer')}}"
 			        >
-                   
-                   
-                    
-
-						
+                   						
             <button type="submit" 
                     
                     class="bg-red-300 text-white rounded ml-1 py-4 px-2 hover:bg-red-500">
@@ -164,23 +147,10 @@
          
         @endif
 
-
     @endforeach           
-       
-       
-      
-
-       
+                       
     </article>
         
-      
-
-    
-
 </div>
-
-
-
-
 
 </x-layout>
