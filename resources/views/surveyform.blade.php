05dc7c5020ac9f68bf70436f3cdef627ab6ab7db
Gilvin Zalsos <gilvinsz@gmail.com>
2022-06-17 22:40:06 +0800

    Paginated Output is a bit normal, and first attempt to align all the shits in tailwind accordingly, GL next

--- resources/views/surveyform.blade.php
+++ resources/views/surveyform.blade.php
@@ -1,46 +1,53 @@
 <x-layout>
 
 
-<div id="body" class="bg-gray-300 flex-auto"> 
+<div id="body" class="bg-gray-300 flex-auto border border-green-500"> 
 
 
-    <article class="bg-gray-500">     
+        <article class="bg-gray-300 p-24 border border-red-500 place-self-start text-left place-content-start place-items-start justify-items-start justify-self-start content-start items-start self-start bottom-3">
+             <strong>   Good Evening, Counselor! </strong>
+        </article>
+
+
    <!--  @foreach($survey as $survey)
     <a href="/createanswer/{{$survey}}"> <button class="bg-yellow-300"> Die and Dump </button> </a>
-    @endforeach -->
-  
+    @endforeach -->  
     @foreach($SurveyQuestions as $SurveyQuestion)
 
     @if($SurveyQuestion->type =='multiplechoice')
-  
+       <article class="bg-gray-300 border border-red-500 p-48">   
     <form method="POST" action="/createanswer/{{$survey}}/{{$SurveyQuestion->id}}/"> 
        @csrf
        @method('POST')
                                           
-                <div class="flex-auto space-y-2 bg-gray-300"> 
+                <div class="flex-auto bg-white"> 
 
   
-                        <table class="border border-red text-white">                     
+                        <table class="border border-black text-black">                     
                                 <tr class="border border-black" >  
-                                        <th> {{$SurveyQuestion->question}} (Multiple Choice) </th> </tr>
+                                        <th class="px-24"> {{$SurveyQuestion->question}} (Multiple Choice) </th> </tr>
                                 @foreach($choices as $choice)
-                                <th> <input type="checkbox" name="pets" value="Dog"> {{$choice->question_choice}} </th> </tr>          
-                                @endforeach
-                                  </table>
                         
-                                                                         						
-            <button type="submit" class="bg-red-300 text-white rounded ml-1 py-4 px-2 hover:bg-red-500"> Submit Answer </button>  
-                     
-                       
-
+                                <th class="px-24"> <input type="checkbox" name="pets" value="Dog"> {{$choice->question_choice}} </th> 
+                                @if($loop->iteration %2 ==0)         
+                                <tr>
+                                        @endif
+                                @endforeach
+                        </table>
+                                                                                						
+                <button type="submit" class="bg-red-300 text-white rounded ml-1 py-4 px-2 hover:bg-red-500"> Submit Answer </button>  
+                                 
                 </div>
-                        <div class="bg-red-300">
-                    {{$SurveyQuestions->links()}}
-                        </div>
+                        
+                   
+                        
                     </form>	
-       
+                    
+                
+        </article>
+{{$SurveyQuestions->links()}}  
         @endif
-
+                
 
      @if($SurveyQuestion->type =='likertscale')
        <form method="POST" action="/createanswer/{{$survey}}/{{$SurveyQuestion->id}}/"> 
