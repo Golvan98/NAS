a334563bbe8ed04f5b7e0a3a66f06189574ec95a
Gilvin Zalsos <gilvinsz@gmail.com>
2022-06-23 07:03:43 +0800

    All Question type Templates, to be polished after data viz and backend, it'll be a masterpiece, GL bro

--- resources/views/surveyform.blade.php
+++ resources/views/surveyform.blade.php
@@ -112,6 +112,8 @@
         </div>
 
                     </form>	
+
+                    {{$SurveyQuestions->links()}}       
         @endif
        
         @if($SurveyQuestion->type =='matrixquestion')
@@ -119,24 +121,31 @@
        @csrf
        @method('POST')
                                             
-                    <div class="flex-auto space-y-2"> 
-                                       
-                    {{$SurveyQuestion->question}}  (Matrix Question)
-                                                          
-                    @foreach($choices as $choice)
-                    <input type="checkbox" name="pets" value="Dog"> {{$choice->question_choice}}<br>
-                  
-                    @endforeach
-                                       						
-            <button type="submit" 
-                    
-                    class="bg-red-300 text-white rounded ml-1 py-4 px-2 hover:bg-red-500">
-                     Submit Answer 
-                     </button>  
-                    </div>
-                    {{$SurveyQuestions->links()}}
+       <div class="flex-auto bg-gray-300"> 
 
-                    </form>	
+                <div class="flex-auto bg-gray-300 border border-red-500">
+
+                        <table class="ml-4 mr-4 border border-black mt-2 bg-white text-black">                
+                
+                        <tr class="">  
+                        <td class="px-1">  <strong> {{$SurveyQuestion->question}} (matrixquestion) lores epsum lores epsum lores epsum </strong> </td> 
+                        @foreach($choices as $choice)
+                                <td colspan="1"class="px-4 py-4 border border-black whitespace-pre font-bold text-start flex-auto"> <input type="checkbox" name="pes" value=""> {{$choice->question_choice}} </input></td>                               
+                        @endforeach
+                        
+                                
+                        </table>
+                        <button class="bg-green-300 text-white rounded ml-1 py-1 px-3 hover:bg-green-500"> Back </button>                 
+
+                        <button type="submit" class="bg-red-300 text-white rounded ml-1 py-1 px-2 hover:bg-red-500"> Submit Answer </button>  
+
+                </div>   
+
+        </div>
+  
+
+                    </form>
+                    {{$SurveyQuestions->links()}} 	
         @endif
 
         @if($SurveyQuestion->type =='openended')
@@ -144,29 +153,25 @@
 <form method="POST" action="/createanswer/{{$survey}}/{{$SurveyQuestion->id}}/"> 
        @csrf
        @method('POST')
-                                       
-                    <div class="flex-auto space-y-2"> 
-                                    
-                    {{$SurveyQuestion->question}} (Open Ended)
-                                                   
-                    <input class="border border-gray-400 p-0.5 w-full"
-			        type="text"
-			        name="answer"
-			        id="answer"
-		        	required
-                    value="{{old('answer')}}"
-			        >
-                   						
-            <button type="submit" 
-                    
-                    class="bg-red-300 text-white rounded ml-1 py-4 px-2 hover:bg-red-500">
-                     Submit Answer 
-                     </button>  
-                    </div>
-                    {{$SurveyQuestions->links()}}
+
+       <div class="flex-auto bg-gray-300"> 
+
+                <div class="flex-auto bg-gray-300 border border-red-500">
+                
+
+                <div class="bg-white content-center items-center text-center border border-black mx-4 my-2">
+            <label for="answer" class="content-center items-center block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"> <strong> {{$SurveyQuestion->question}} (Open Ended)  </strong> </label>
+            <input type="text" id="answer" class="my-1 px-2 content-center items-center bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block hover w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required value="{{old('answer')}}"> 
+                </div>
+                        <button class="bg-green-300 text-white rounded ml-1 py-1 px-3 hover:bg-green-500"> Back </button>                 
+                        <button type="submit" class="bg-red-300 text-white rounded ml-1 py-1 px-2 hover:bg-red-500"> Submit Answer </button>  
+
+                </div>  
+
+        </div>
 
                     </form>	
-         
+                    {{$SurveyQuestions->links()}}
         @endif
 
     @endforeach           
