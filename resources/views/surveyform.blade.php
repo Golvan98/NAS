f104d5e6106d980d62d36305d55d29d83c22840e
Gilvin Zalsos <gilvinsz@gmail.com>
2022-06-30 18:37:19 +0800

    The Product Finally Perfected. LeL Multiple Choice is done af. Finally

--- resources/views/surveyform.blade.php
+++ resources/views/surveyform.blade.php
@@ -6,53 +6,59 @@
     @if($SurveyQuestion->type =='multiplechoice')
 
     
-<div class="w-full bg-gray-300 border border-red-500">
+<div class="w-full bg-gray-300">
 
         <div class="flex justify-start w-full h-1/4 items-end bg-gray-300 border border-red-500">
-           <div class="ml-12">Good Day, Counselor! </div>
+                <div class="ml-12">Good Day, Counselor! </div>
         </div>
 
-   <div class="flex justify-center w-full h-3/4 bg-gray-300 border border-green-500"> 
-        
-        <form method="POST" action="/createanswer/{{$survey}}/{{$SurveyQuestion->id}}/">
-        <div class="mt-4 mr-4 flex-nowrap w-full h-5/6 bg-white border border-yellow-500">  
-           @csrf @method('POST')  
 
-        <table>
+        <div class="flex justify-center w-full h-3/4 bg-gray-300 border border-green-500"> 
+        <form method="POST" action="" class=" mt-4 mx-4 flex-nowrap w-full h-5/6 bg-white border border-green-500">  
+        @csrf @method('POST')  
 
-                <div class="flex justify-center items-center bg-gray-500 w-full h-1/6">
-                   <div class="">  {{$SurveyQuestion->question}} Multiple Choice ni Sir </div> 
-                </div>
+ 
+  <div class="flex-nowrap bg-white w-full h-5/6">
+    <table>
 
-               
-                <div class="flex justify-between w-full h-auto bg-red-100">
-                @foreach($choices as $choice)    <div class=""> <input type="checkbox"> {{$choice->question_choice}} </div>
+       <tr>
+        <div class="flex justify-center items-center bg-white w-full h-auto h-2/6 "> {{$SurveyQuestion->question}} (Multiple Choice) </div>
+
+        <div class="flex bg-gray-300 h-1/6"> 
+
+          @foreach($choices as $choice)
+        <div class="flex justify-center items-center bg-red-300 px-4 py-1 w-1/3 h-auto border border-white"> <input type="checkbox"> {{$choice->question_choice}} </div> 
+          @endforeach          
+
+        </div>
+
+        <div class="flex bg-gray-300 h-1/6">   
+            <div class="flex justify-center items-center bg-red-300 px-4 py-1 w-1/3 h-auto border border-white"> <input type="checkbox"> MipMerp </div>
+
+            <div class="flex justify-center items-center bg-red-300 px-4 py-1 w-1/3 h-auto border border-white"> <input type="checkbox"> MipMerp </div>
+        </div>
+
+
+
+    </table>
+  </div>
+
+        <div class="flex justify-between w-full h-1/6 bg-red-100">
+                <div class=""> <button class="px-8 py-2 bg-red-500 rounded-xl"> Back </div> 
+                <div class=""> <button class="px-8 py-2 bg-blue-300 rounded-xl"> Edit </div> 
+                <div class=""> <button class="px-8 py-2 bg-green-500 rounded-xl"> Next </div> 
                 
-                @if($loop->iteration %3==0)
-                </div> 
-                 <div class="flex justify-between w-full h-auto bg-red-100">
-                @endif
-                @endforeach  
-                </div>
-                
-        
-                <div class="flex justify-between w-full h-auto bg-red-100">
-                        <div class=""> <input type="checkbox"> 02 </div> 
-                        <div class="">  <input type="checkbox"> 02 </div> 
-                        <div class="">  <input type="checkbox"> 02 </div> 
-                </div>
+        </div>
+         
+         <div class="mt-1"> {{$SurveyQuestions->links()}}  </div>
 
-        <table>
+ 
         </div>
         </form>
-
        
-
-   </div>
-
-      
 </div>
 
+        
         
 
     @endif
