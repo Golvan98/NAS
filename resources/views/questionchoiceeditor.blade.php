daf9864f4c14dd74729b14ae112ab1216e3a9e13
Gilvin Zalsos <gilvinsz@gmail.com>
2022-07-09 19:50:28 +0800

    July 9 Erm, Forced editing of the question choice landing page front-end

--- resources/views/questionchoiceeditor.blade.php
+++ resources/views/questionchoiceeditor.blade.php
@@ -3,10 +3,10 @@
 
 
 
-<div class="w-full bg-gray-300 flex justify-center items-center">
+<div class="w-full h-full bg-gray-300 flex justify-center items-center">
 
     
-    <div class="w-3/4 h-3/4 bg-gray-500 items-center border border-black">
+    <div class="w-3/4 bg-gray-100 border border-black mt-16 mb-12">
         
         
 
@@ -14,21 +14,19 @@
           
             @foreach($QuestionChoices as $QuestionChoice)
             
-            <div class="w-full flex justify-between mt-2">
+            <div class="w-full flex justify-between mt-4">
 
              <div class="w-1/3 text-center"> <a href="#"> <button class="bg-gray-300 px-4 py-1 rounded-xl">   {{ $QuestionChoice->question_choice }}  </button> </a> </div>
-             <div class="w-1/3 text-center"> <a href="/questionchoiceedit/{{$QuestionChoice->id}}> <button class="bg-gray-300 px-4 py-1 rounded-xl">   Edit  </button> </a> </div>
+             <div class="w-1/3 text-center"> <a href="/questionchoiceedit/{{$QuestionChoice->id}}"> <button class="bg-gray-300 px-4 py-1 rounded-xl">   Edit  </button> </a> </div>
              <div class="w-1/3 text-center"> <a href="#"> <button class="bg-red-300 px-4 py-1 rounded-xl">   Delete </button> </a> </div> 
-
-             
-             
+                          
             </div>
             @endforeach
 
 
             
 
-            <div class=" w-full h-1/2 flex items-end mt-6"> 
+            <div class="w-full h-full flex items-end mt-6 mr-2"> 
 
               <div class="w-full bg-gray-300"> {{$QuestionChoices->links()}} </div>
 
@@ -37,10 +35,7 @@
 
 
 
-          
-            
-            
-         
+                                           
 
     </div>
     
