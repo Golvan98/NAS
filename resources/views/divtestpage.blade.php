ddf6678800d60c5540330caa80d416c2a15d7ee4
Gilvin Zalsos <gilvinsz@gmail.com>
2022-07-21 07:23:09 +0800

    July 21 Modal with Props used as passing views from component to component

--- resources/views/divtestpage.blade.php
+++ resources/views/divtestpage.blade.php
@@ -13,10 +13,10 @@
     @foreach($surveys as $survey)
     <div class="w-full h-1/6 flex justify-between bg-red-300 items-center "> 
 
-      <div class="w-1/5 flex justify-center "> <button class="bg-gray-100 px-4 py-2 rounded-xl">  {{$survey->name}}</button> </div>
-      <div class="w-1/5 flex justify-center "> <button class="bg-gray-200 px-4 py-2 rounded-xl">  Answer Survey </button> </div>
-      <div class="w-1/5 flex justify-center "> <button class="bg-gray-300 px-4 py-2 rounded-xl">  Manage Survey </button> </div>
-      <div class="w-1/5 flex justify-center "> <button class="bg-gray-400 px-4 py-2 rounded-xl">  Edit Questions </button> </div>
+      <div class="w-1/5 flex justify-center "> <button class="bg-gray-100 px-4 py-2 rounded-xl">  {{$survey->name}}</button> </div> 
+      <div class="w-1/5 flex justify-center "> <a href="/surveyform/{{$survey->id}}"> <button class="bg-gray-200 px-4 py-2 rounded-xl">  Answer Survey </button> </a> </div>
+      <div class="w-1/5 flex justify-center "> <x-surveymodal survey="{{$survey->id}}"> </x-surveymodal> </div>
+      <div class="w-1/5 flex justify-center "> <a href="/questionlist/{{$survey->id}}"> <button class="bg-gray-400 px-4 py-2 rounded-xl">  Edit Questions </button> </a> </div>
       <div class="w-1/5 flex justify-center "> <button class="bg-gray-500 px-4 py-2 rounded-xl">  Create Survey </button> </div>
 
     </div>
