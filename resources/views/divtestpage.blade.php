3b6cffd175f9a63e96671b5a28d33c0467dac715
Gilvin Zalsos <gilvinsz@gmail.com>
2022-07-22 01:25:41 +0800

    Pagination UI Survey Management Page

--- resources/views/divtestpage.blade.php
+++ resources/views/divtestpage.blade.php
@@ -11,13 +11,13 @@
     </div>
 
     @foreach($surveys as $survey)
-    <div class="w-full h-1/6 flex justify-between bg-red-300 items-center "> 
+    <div class="w-full h-1/6 flex justify-between bg-red-300 items-center space-y-1 "> 
 
-      <div class="w-1/5 flex justify-center "> <button class="bg-gray-100 px-4 py-2 rounded-xl">  {{$survey->name}}</button> </div> 
-      <div class="w-1/5 flex justify-center "> <a href="/surveyform/{{$survey->id}}"> <button class="bg-gray-200 px-4 py-2 rounded-xl">  Answer Survey </button> </a> </div>
+      <div class="w-1/5 flex justify-center "> <button class="bg-gray-100 px-4 py-1 rounded-xl">  {{$survey->name}}</button> </div> 
+      <div class="w-1/5 flex justify-center "> <a href="/surveyform/{{$survey->id}}"> <button class="bg-gray-200 px-4 py-1 rounded-xl">  Answer Survey </button> </a> </div>
       <div class="w-1/5 flex justify-center "> <x-surveymodal survey="{{$survey->id}}" surveyname="{{$survey->name}}"> </x-surveymodal> </div>
-      <div class="w-1/5 flex justify-center "> <a href="/questionlist/{{$survey->id}}"> <button class="bg-gray-400 px-4 py-2 rounded-xl">  Edit Questions </button> </a> </div>
-      <div class="w-1/5 flex justify-center "> <button class="bg-gray-500 px-4 py-2 rounded-xl">  Create Survey </button> </div>
+      <div class="w-1/5 flex justify-center "> <a href="/questionlist/{{$survey->id}}"> <button class="bg-gray-400 px-4 py-1 rounded-xl">  Edit Questions </button> </a> </div>
+      <div class="w-1/5 flex justify-center "> <button class="bg-gray-500 px-4 py-1 rounded-xl">  Create Survey </button> </div>
 
     </div>
     @endforeach
@@ -28,9 +28,16 @@
       <div class="w-1/5 flex justify-center "> <x-createsurveymodal> </x-createsurveymodal> </div> 
 
     </div> 
+
+    <div class="w-full h-1/7 items-center mt-1">
+
+       {{$surveys->links()}} 
+  
+    </div> 
       
 
   </div>
+  
 
      
 </div>
