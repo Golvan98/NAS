95e0543d0015cca6a0d09a89b0de9be2a5faa3ba
Gilvin Zalsos <gilvinsz@gmail.com>
2022-07-22 08:11:14 +0800

    Polished Edit and Delete Modal for Survey July 22

--- resources/views/components/surveymodal.blade.php
+++ resources/views/components/surveymodal.blade.php
@@ -21,7 +21,7 @@
                                 @method('PATCH')
                                   <div>
                                       <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"> Question Choice</label>
-                                      <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"  placeholder="{{$surveyname}}" required>
+                                      <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"  value="{{$surveyname}}" required>
                                   </div>
 
                                   <div class="flex justify-between">
