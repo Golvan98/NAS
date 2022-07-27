ec2c95c6067363d5ddb6647360f21489dc102f9d
Gilvin Zalsos <gilvinsz@gmail.com>
2022-07-28 07:39:40 +0800

    div template up to back and add button july 28

--- resources/views/divtestpage2.blade.php
+++ resources/views/divtestpage2.blade.php
@@ -1,10 +1,36 @@
 <x-layout>
     <script src="../path/to/flowbite/dist/flowbite.js"></script>
 
-<div class="w-full bg-gray-300 flex justify-center items-center border border-red-500">
+<div class="w-full bg-blue-300 flex justify-center items-center">
+
+    <div class="w-4/6 h-4/6 bg-red-300 flex-nowrap ">
+
+      <div class="w-full h-5/6 border border-black flex-nowrap">
+
+        <div class="w-full flex justify-center h-1/5 items-center"> Question Choice Name Here </div>
+
+        <div class="w-full flex justify-center h-1/5 items-center"> line 1 </div>
+
+        <div class="w-full flex justify-center h-1/5 items-center"> line 1 </div>
+
+        <div class="w-full flex justify-center h-1/5 items-center">  </div>
+
+        <div class="w-full flex justify-center h-1/5 items-center">  </div>
+        
+      </div>
+
+      <div class="w-full/6 h-1/6 bg-blue-300 flex justify-between border border-white"> 
+
+        <button> Back  </button> 
+        <button> Back Line </button> 
+
+      </div>
+           
+      <div class="w-full/6 h-1/5 bg-green-300 flex justify-between border border-white"> Pagination </div>
+    
+    </div>
 
    
-
 </div>
 
 </x-layout>
