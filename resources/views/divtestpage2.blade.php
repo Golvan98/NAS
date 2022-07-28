322f4e63b0f3db9a1714dd8767d57facf63123c4
Gilvin Zalsos <gilvinsz@gmail.com>
2022-07-28 19:50:21 +0800

    Alright, I think this is the div u want

--- resources/views/divtestpage2.blade.php
+++ resources/views/divtestpage2.blade.php
@@ -1,35 +1,28 @@
 <x-layout>
     <script src="../path/to/flowbite/dist/flowbite.js"></script>
 
-<div class="w-full bg-gray-300 flex justify-center items-center">
+<div class="w-full bg-gray-300 flex-nowrap ">
 
+    <div class="w-full h-5/6 bg-gray-500 flex justify-center items-end">
 
-    <div class="w-full h-5/6 bg-red-300 flex-nowrap border border-white">
+        <div class="w-4/6 h-5/6 bg-red-300 flex-nowrap border border-white">
 
-        <div class="w-full h-4/6 bg-gray-500 flex justify-center items-center"> 
-
-            <div class="w-4/6 h-full border border-black">
-
-                <div class="w-full flex justify-center h-1/5 items-center"> Question Choice Name Here </div>
-
-                <div class="w-full flex justify-center h-1/5 items-center"> line 1 </div>
-
-                <div class="w-full flex justify-center h-1/5 items-center"> line 1 </div>
-
-                <div class="w-full flex justify-center h-1/5 items-center">  </div>
-
-                <div class="w-full flex justify-center h-1/5 items-center">  </div>
-                
-            </div>
-         
+            
         </div>
 
-        <div class="w-full h-1/6 bg-gray-500 flex justify-center items-center"> Back and Add Button here </div>
-
-        <div class="w-full h-1/6 bg-gray-500 flex justify-center items-center"> Pagination Link here </div>
-      
     </div>
 
+    <div class="w-full h-1/6 bg-blue-300 flex-nowrap">
+
+        <div class="w-full h-1/2 bg-gray-300 flex justify-center items-center"> 
+
+        </div>
+
+
+        <div class="w-full h-1/2 bg-red-300 flex justify-center items-center">
+
+        </div>
+    </div>
 
     
     
