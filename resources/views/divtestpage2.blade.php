0fdd8ff577ffdc0d794c1fbf04ea3fd6c89dc7eb
Gilvin Zalsos <gilvinsz@gmail.com>
2022-07-28 19:21:08 +0800

    Man.. Idk anymore, but here's a commit that's ALMOST close in creating the div template that I wanted... July 28

--- resources/views/divtestpage2.blade.php
+++ resources/views/divtestpage2.blade.php
@@ -1,35 +1,38 @@
 <x-layout>
     <script src="../path/to/flowbite/dist/flowbite.js"></script>
 
-<div class="w-full bg-blue-300 flex justify-center items-center">
+<div class="w-full bg-gray-300 flex justify-center items-center">
 
-    <div class="w-4/6 h-4/6 bg-red-300 flex-nowrap ">
 
-      <div class="w-full h-5/6 border border-black flex-nowrap">
+    <div class="w-full h-5/6 bg-red-300 flex-nowrap border border-white">
 
-        <div class="w-full flex justify-center h-1/5 items-center"> Question Choice Name Here </div>
+        <div class="w-full h-4/6 bg-gray-500 flex justify-center items-center"> 
 
-        <div class="w-full flex justify-center h-1/5 items-center"> line 1 </div>
+            <div class="w-4/6 h-full border border-black">
 
-        <div class="w-full flex justify-center h-1/5 items-center"> line 1 </div>
+                <div class="w-full flex justify-center h-1/5 items-center"> Question Choice Name Here </div>
 
-        <div class="w-full flex justify-center h-1/5 items-center">  </div>
+                <div class="w-full flex justify-center h-1/5 items-center"> line 1 </div>
 
-        <div class="w-full flex justify-center h-1/5 items-center">  </div>
-        
-      </div>
+                <div class="w-full flex justify-center h-1/5 items-center"> line 1 </div>
 
-      <div class="w-full/6 h-1/6 bg-blue-300 flex justify-between border border-white"> 
+                <div class="w-full flex justify-center h-1/5 items-center">  </div>
 
-        <button> Back  </button> 
-        <button> Back Line </button> 
+                <div class="w-full flex justify-center h-1/5 items-center">  </div>
+                
+            </div>
+         
+        </div>
 
-      </div>
-           
-      <div class="w-full/6 h-1/5 bg-green-300 flex justify-between border border-white"> Pagination </div>
-    
+        <div class="w-full h-1/6 bg-gray-500 flex justify-center items-center"> Back and Add Button here </div>
+
+        <div class="w-full h-1/6 bg-gray-500 flex justify-center items-center"> Pagination Link here </div>
+      
     </div>
 
+
+    
+    
    
 </div>
 
