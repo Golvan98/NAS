ee3d7081e50aac69c7308b61affd7afb70d19b98
Gilvin Zalsos <gilvinsz@gmail.com>
2022-08-06 11:58:36 +0800

    template for pie chart and bar chart thingy Aug 5

--- resources/views/divtestpage.blade.php
+++ resources/views/divtestpage.blade.php
@@ -1,18 +1,18 @@
-<x-newlayout>
+<x-layout>
 
-<div id="main" class="flex-nowrap w-full h-full bg-gray-500">
+<div id="main" class="flex-nowrap w-full h-full bg-gray-300">
 
 
     <div class="w-full bg-white flex h-1/6 justify-center items-end  ">
         
        <div class="w-full flex-nowrap ">
         
-            <div class="w-full flex justify-center"> 
+            <div class="w-full flex justify-center items-end"> 
                 <div class="w-4/5 text-lg"> <strong> Good Evening, Counselor! </strong> </div>
             </div>
 
             <div class="w-full flex justify-center"> 
-                <div class="w-5/6 h-0.5 bg-red-500">  </div>
+                <div class="w-full h-0.5 bg-red-500 mx-2">  </div>
             </div>
        </div>
        
@@ -20,48 +20,33 @@
     </div>
     
     
-    <div class="w-full bg-gray-300 flex h-5/6 ull justify-center items-center  ">
+    
+    
+    <div class="w-full bg-gray-300 flex h-full justify-center items-center h-4/6">
 
 
+        <div class="flex w-full h-full bg-gray-300 border border-black mt-8">
 
-        <div class="flex-nowrap w-4/5 h-auto bg-gray-300 border border-black">
-
-            <div class="w-full h-1/6 flex justify-center bg-gray-300 items-center "> 
+            <div class="w-1/6 h-full flex justify-center bg-gray-300 items-center border border-red-500 "> 
             
                 <div> <strong> Good Day, Counselor! </strong> </div>
 
             </div>
 
-            @foreach($surveys as $survey)
-            <div class="w-full h-1/6 flex justify-between bg-gray-300 items-center space-y-10 "> 
-
-                <div class="w-1/5 flex justify-center "> <button class="bg-gray-100 px-4 py-1 rounded-xl">  {{$survey->name}}</button> </div> 
-                <div class="w-1/5 flex justify-center "> <a href="/surveyform/{{$survey->id}}"> <button class="bg-gray-200 px-4 py-1 rounded-xl">  Answer Survey </button> </a> </div>
-                <div class="w-1/5 flex justify-center "> <x-surveymodal survey="{{$survey->id}}" surveyname="{{$survey->name}}"> </x-surveymodal> </div>
-                <div class="w-1/5 flex justify-center "> <a href="/questionlist/{{$survey->id}}"> <button class="bg-gray-400 px-4 py-1 rounded-xl">  Edit Questions </button> </a> </div>
-                
-                
-            </div>
-            @endforeach
-
-            <div class="w-full h-1/6 flex justify-between bg-gray-300 items-center ">
-
-                <div class="w-1/5 flex justify-center "> <a href="/home"> <button class="bg-red-500 px-8 py-1 rounded-xl">  Back </button> </a> </div> 
-                <div class="w-1/5 flex justify-center "> <x-createsurveymodal> </x-createsurveymodal> </div> 
-
-            </div> 
-
-            <div class="w-full bg-transparent h-1/6 flex-nowrap">
-
-                <div class="w-full h-4/5 flex justify-center items-center bg-transparent">
-
-                    <div class="w-full h-4/6 text-white mt-2">
-                    {{$surveys->links()}} 
-                    </div>
-                
-                </div>
+            <div class="w-4/6 h-full flex justify-center bg-gray-300 items-center border border-red-500 "> 
+            
+                <div> <strong>   <img class="object-fill w-full h-full" src="{{ asset('storage/dummypie.png') }}" alt="description of myimage"> </strong> </div>
 
             </div>
+
+            <div class="w-1/6 h-full flex-nowrap justify-center bg-gray-300 items-center border border-red-500 "> 
+            
+                <div> <strong> Print Button Here </strong> </div>
+
+                <div class="mt-48"> <strong> Next Picture for bar/pie chart here</strong> </div>
+
+            </div>
+           
         
 
         </div>
@@ -74,4 +59,4 @@
 
 </div>
         
-</x-newlayout>
+</x-layout>
