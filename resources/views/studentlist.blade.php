050e8b816ccc7ad72b35aeaaa503d2738ea380b1
Gilvin Zalsos <gilvinsz@gmail.com>
2022-08-10 20:28:35 +0800

    template update for student list

--- resources/views/studentlist.blade.php
+++ resources/views/studentlist.blade.php
@@ -22,21 +22,41 @@
     
     
     
-    <div class="w-full bg-gray-300 flex h-full justify-center items-center h-4/6">
+    <div class="w-full bg-gray-300 h-full flex justify-center items-center h-4/6">
 
 
         <div class="flex w-full h-full bg-gray-300 border border-black mt-8">
 
-            <div class="w-1/6 h-full flex justify-center items-end bg-white-300 items-center border border-red-500"> 
+            <div class="w-1/6 h-full flex justify-start items-end border border-red-500"> 
             
-                <div> <button class="bg-green-400 text-white font-bold px-10 py-1"> back button here </button> </div>
+                <div> 
+
+                    <button class="bg-green-400 text-white font-bold px-10 py-1 ml-4 mb-2"> Back </button> 
+
+                </div>
 
             </div>
 
-            <div class="w-4/6 h-full flex justify-center bg-gray-300 items-center border border-red-500 "> 
+            <div class="w-4/6 h-full bg-gray-300 items-center border border-red-500 "> 
             
-                <div> <strong>  student list here center </strong> </div>
+                <div class="flex-nowrap w-full h-full bg-gray-300"> 
+        
+                   <div class="flex-nowrap h-1/6"> 
 
+                        <div class="flex justify-center items-center h-2/3 bg-red-300 text-lg font-bold"> School of Computer Studies </div>
+                        <div class="flex justify-center h-1/3 bg-red-300"> Motivation </div>
+
+                   </div>
+
+                   <div class="flex-nowrap items-start h-5/6"> 
+
+                    <div class="justify-center"> 1 </div>
+                    <div class="justify-center"> 2 </div>
+
+                    </div>
+
+                </div>
+                
             </div>
 
             <div class="w-1/6 h-full flex-nowrap justify-center bg-gray-300 items-center border border-red-500 "> 
