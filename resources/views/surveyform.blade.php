f486c841cb49dcfb4f86e6868e30533cd0cd0c9e
Gilvin Zalsos <gilvinsz@gmail.com>
2022-08-25 18:51:25 +0800

    Still on this shit, trying to create an algo in controller now where u compare answer chice and question choice then do a check if values are same, Aug 25

--- resources/views/surveyform.blade.php
+++ resources/views/surveyform.blade.php
@@ -166,9 +166,15 @@
 
                 @foreach($choices as $choice)     
 
-                <div class="flex flex-wrap  bg-gray-300 px-2 py-1 w-1/5  h-auto truncate hover:text-clip border border-black"> <input type="checkbox"  name="question_choice[]"  id="question_choice[]" value="{{$choice->question_choice}}" @if {{$request()->get('question_choice')}} == $choice->question_choice) checked @endif> &nbsp {{$choice->question_choice}} </div> 
-                                                                                                                                                                                                                 
+                @php
+                $option = $choice->question_choice;
+                @endphp
+                <div class="flex flex-wrap  bg-gray-300 px-2 py-1 w-1/5  h-auto truncate hover:text-clip border border-black"> <input type="checkbox"  name="question_choice[]"  id="question_choice" value="{{$choice->question_choice}}" @if(old('question_choice') == $option))  checked  @endif> &nbsp {{$choice->question_choice}} </div> 
+                                                                                    
+                
                 @endforeach
+
+                
                 </div>
 
 
