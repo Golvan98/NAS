84953e3350919100a8215e664d170c48bd447d4c
Gilvin Zalsos <gilvinsz@gmail.com>
2022-09-21 19:11:22 +0800

    Right side div for dynamic pie chart page, line graph is to be added here September 21

--- resources/views/dynamicpiechart.blade.php
+++ resources/views/dynamicpiechart.blade.php
@@ -153,7 +153,19 @@
                 
             </div>
 
-            <div id="main box" class="w-1/6 bg-white flex h-full">
+            <div id="main box" class="flex-nowrap w-1/6 bg-white flex h-full">
+
+                <div class="bg-red-300 w-full h-full flex-nowrap"> 
+
+                    <div class="bg-white w-full h-1/6 flex justify-end"> 
+
+                       <div class="w-full h-full"> <button class=" ml-16 mt-4 bg-gray-900 px-10 py-2 text-white font-bold rounded-md "> Print </button> </div>
+
+                    </div>
+
+                    <div class="bg-white w-full h-5/6"> box 2</div>
+
+                </div>
 
             </div>
 
